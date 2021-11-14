ad4a39fde6fee8572be58b11de42f476a6a87b75
kamaal111 <kamaal.f1@gmail.com>
2021-11-14 16:19:55 +0100

    Renaming content.js to githubContent.js

--- Extensions/Safari/Shared/Resources/background.js
+++ Extensions/Safari/Shared/Resources/background.js
@@ -1,6 +1,6 @@
 // background.js
 
-function handleContentFound(sender, sendResponse) {
+function handleGithubContentFound(sender, sendResponse) {
   if (sender?.origin !== 'https://github.com' && sender.url != null) return;
 
   sendResponse({ type: 'github-url', url: sender });
@@ -12,8 +12,8 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request?.type == null) return;
 
   switch (request.type) {
-    case 'content-found':
-      handleContentFound(sender, sendResponse);
+    case 'github-content-found':
+      handleGithubContentFound(sender, sendResponse);
       break;
     default:
       break;
